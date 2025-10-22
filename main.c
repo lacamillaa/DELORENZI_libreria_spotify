3c5e6db0e2d8b86e31b7c8705eb3fe4a34d5a63e
lacamillaa <12999356@itisrossi.vi.it>
2025-10-22 11:24:31 +0200

    inserimento delle stringhe

--- main.c
+++ main.c
@@ -8,6 +8,26 @@ typedef struct {
     int seconds;
 } Song;
 
+char* insertString(int maxLen) {
+    int i = 0;
+    char c = 0;
+    char* arr = (char*) malloc((maxLen + 1) * sizeof(char));
+    while (i < maxLen && c != '\n') {
+        c = (char)getchar();
+        arr[i] = c;
+        i++;
+    }
+    arr[i] = '\0';
+    char *newString = (char*)malloc(i * sizeof(char));
+    for (int k = 0; k <= i; k++) {
+        newString[k] = arr[k];
+    }
+    free(arr);
+    arr = NULL;
+    fflush(stdin);
+    return newString;
+}
+
 int main(void) {
     short int scelta = 0;
     int nSongs = 0;
@@ -32,7 +52,19 @@ int main(void) {
                 if (songs == NULL) {
                     printf("No memory left\n");
                 }
-                // creazione della canzone
+                else {
+                    // creazione della canzone
+                    Song *song = (Song*) malloc(sizeof(Song));
+                    if (song == NULL) {
+                        printf("No memory left\n");
+                    }
+                    else {
+                        char* nome = insertString(100);
+                        char* artist = insertString(100);
+                        char* album = insertString(100);
+                        char* seconds = insertString(5);
+                    }
+                }
             }
             else if (scelta == '2') {
                 // stampa
